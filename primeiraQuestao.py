7c73928d0b169bd0ff0c3735d773296100520999
Rafael A <rafaelcardosocatuti@gmail.com>
2024-04-06 18:35:35 -0300

    fix: primeiraQuestao

--- primeiraQuestao.py
+++ primeiraQuestao.py
@@ -3,10 +3,6 @@ soma = 0
 k = 0
 
 while k < indice:
-    print("k antes: " + str(k)) 
     k=k+1
-    print("k depois: " + str(k))
-    print("soma antes: " + str(soma))
     soma=soma+k
-    print("soma depois: " + str(soma))
 print(soma)
